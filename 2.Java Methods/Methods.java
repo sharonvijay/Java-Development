f509a5a84dc94d5159d650c53012217d4f1ddaab
sharonvijay <94788632+sharonvijay@users.noreply.github.com>
2023-05-16 01:21:07 +0530

    commit

---
+++ 2.Java Methods/Methods.java
@@ -0,0 +1,10 @@
+public class Methods {
+    public void myMethod() {
+        System.out.println("Example of Java methods");
+    }
+
+    public static void main(String[] args) {
+        Methods obj = new Methods();
+        obj.myMethod();
+    }
+}
