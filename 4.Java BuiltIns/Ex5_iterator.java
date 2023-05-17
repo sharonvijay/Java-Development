4cdaf1efb2b5ec1e5b645fd23dd80ab4dfd73780
sharonvijay <94788632+sharonvijay@users.noreply.github.com>
2023-05-18 01:58:50 +0530

    Iterator

---
+++ 4.Java BuiltIns/Ex5_iterator.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Ex5_iterator {
+    public static void main(String[] args) {
+
+        // Make a collection
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+
+        // Get the iterator
+        Iterator<String> it = cars.iterator();
+
+        // Print the first item
+        System.out.println(it.next());
+
+        // Loop through a collection
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
