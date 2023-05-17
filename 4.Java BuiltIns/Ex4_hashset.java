b24e9bedd8f6520810194ad70e697b740ad029c0
sharonvijay <94788632+sharonvijay@users.noreply.github.com>
2023-05-18 01:54:59 +0530

    HashSet

---
+++ 4.Java BuiltIns/Ex4_hashset.java
@@ -0,0 +1,30 @@
+
+// Import the HashSet class
+import java.util.HashSet;
+
+public class Ex4_hashset {
+    public static void main(String[] args) {
+        HashSet<String> cars = new HashSet<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("BMW");
+        cars.add("Mazda");
+        System.out.println(cars);
+
+        // HashSet Size
+        cars.size();
+
+        // Loop Through
+        for (String i : cars) {
+            System.out.println(i);
+        }
+
+        // Check If an Item Exists
+        cars.contains("Mazda");
+
+        // Remove an Item
+        cars.remove("Volvo");
+
+    }
+}
